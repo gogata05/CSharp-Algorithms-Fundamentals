1ba96e9ce173d6b07aef6017a4cc05fb0cee6c13
gogata05 <33290584+gogata05@users.noreply.github.com>
2024-04-15 20:33:02 +0300

    Recursive Array Sum

---
+++ 01. Recurtion and Backtracking - lab/01.Recursice Array Sum/Program.cs
@@ -0,0 +1,14 @@
+﻿var array = Console.ReadLine()!.Split(' ').Select(int.Parse).ToArray();
+
+
+Console.WriteLine(SumArr(array, 0));
+
+static int SumArr(int[] array, int index)
+{
+    if (index == array.Length - 1)
+    {
+        return array[index];
+    }
+
+    return SumArr(array, index + 1) + array[index];
+}
