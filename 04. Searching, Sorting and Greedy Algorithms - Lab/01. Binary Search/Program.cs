b722508838d1655dd72064da7385a1b032b5aa7b
Georgi Markov <33290584+gogata05@users.noreply.github.com>
2024-04-16 11:48:51 +0300

    Binary Search

---
+++ 04. Searching, Sorting and Greedy Algorithms - Lab/01. Binary Search/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace _01._Binary_Search
+{
+    using System;
+    using System.Linq;
+
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+           var arr = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int n = int.Parse(Console.ReadLine());
+            
+            int index = FindIndex(n, arr);
+
+            Console.WriteLine(index);
+        }
+
+        private static int FindIndex(int n, int[] arr)
+        {
+            int start = 0;
+            int end = arr.Length - 1;
+
+            while (start <= end)
+            {
+                var middle = (start + end) / 2;
+
+                if (n == arr[middle])
+                {
+                    return middle;
+                }
+
+                if (n < arr[middle])
+                {
+                    end = middle - 1;
+                }
+                else
+                {
+                    start = middle + 1;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
